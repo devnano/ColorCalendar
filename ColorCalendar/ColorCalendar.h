7da639d935d355c7a98b8b8456b18c74cb3f3b35
Mariano Heredia <mariano@kartjuba.com>
2016-12-17 16:09:35 -0300

    Initial commit.

---
+++ ColorCalendar/ColorCalendar.h
@@ -0,0 +1,19 @@
+//
+//  ColorCalendar.h
+//  ColorCalendar
+//
+//  Created by Mariano Heredia on 12/12/16.
+//  Copyright © 2016 Kartjuba. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ColorCalendar.
+FOUNDATION_EXPORT double ColorCalendarVersionNumber;
+
+//! Project version string for ColorCalendar.
+FOUNDATION_EXPORT const unsigned char ColorCalendarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ColorCalendar/PublicHeader.h>
+
+
---
+++ ColorCalendar/ColorCalendar.swift
@@ -0,0 +1,12 @@
+//
+//  ColorCalendar.swift
+//  ColorCalendar
+//
+//  Created by Mariano Heredia on 12/17/16.
+//  Copyright © 2016 Kartjuba. All rights reserved.
+//
+
+import UIKit
+
+class ColorCalendar:UIView {
+}

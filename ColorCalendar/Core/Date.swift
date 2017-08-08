d1db7530ac70ebddc479a51f8e9b51cbd75f088a
Mariano Heredia <mariano@kartjuba.com>
2017-08-08 19:43:31 +1200

    Date related methods: created Date and extracted funcionality from CalendarHighlight.

---
+++ ColorCalendar/Core/Date.swift
@@ -0,0 +1,42 @@
+//
+//  Date.swift
+//  ColorCalendar
+//
+//  Created by Mariano Heredia on 8/08/17.
+//  Copyright © 2017 Kartjuba. All rights reserved.
+//
+
+import Foundation
+
+extension Date {
+    public func monthName(withLocale locale: Locale) -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MMMM"
+        formatter.locale = locale
+        
+        return formatter.string(from: self).capitalized
+    }
+    
+    public func year(withCalendar calendar: Calendar) -> Int {
+        let year = calendar.component(.year, from: self)
+        
+        return year
+    }
+    
+    public func dayOrdinal(withCalendar calendar: Calendar) -> String {
+        let formatter = NumberFormatter()
+        let day = calendar.component(.day, from: self)
+        formatter.numberStyle = .ordinal        
+        
+        return formatter.string(from: day as NSNumber)!
+    }
+    
+    public func full(withLocale locale: Locale) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .full
+        dateFormatter.doesRelativeDateFormatting = true
+        dateFormatter.locale = locale
+        
+        return dateFormatter.string(from: self)
+    }
+}
---
+++ ColorCalendarTests/DateTests.swift
@@ -0,0 +1,64 @@
+//
+//  DateTests.swift
+//  ColorCalendar
+//
+//  Created by Mariano Heredia on 8/08/17.
+//  Copyright © 2017 Kartjuba. All rights reserved.
+//
+
+import XCTest
+@testable import ColorCalendar
+
+class DateTests: XCTestCase {
+    
+    var date: Date!
+    var locale: Locale!
+    var calendar: Calendar!
+    
+    
+    override func setUp() {
+        super.setUp()
+        locale = Locale(identifier: "EN_us")
+        calendar = Calendar(identifier: Calendar.Identifier.gregorian)
+        super.setUp()
+        createDate()
+        
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testMonthName() {
+        XCTAssert(date.monthName(withLocale: locale) == "August")
+    }
+    
+    func testMonthNameEs() {
+        XCTAssert(date.monthName(withLocale: Locale(identifier: "ES_es")) == "Agosto")
+    }
+    
+    func testYear() {
+        XCTAssert(date.year(withCalendar: calendar) == 2017)
+    }
+    
+    func testDayOrdinal() {
+        XCTAssert(date.dayOrdinal(withCalendar: calendar) == "8th")
+    }
+    
+    func testFull() {
+        createDate(day: 10)
+        XCTAssertEqual(date.full(withLocale: locale), "Thursday, August 10, 2017")
+    }
+    
+    func testFullSpanish() {
+        createDate(day: 10)
+        XCTAssertEqual(date.full(withLocale: Locale(identifier: "ES_es")), "pasado mañana")
+    }
+    
+    private func createDate(year: Int = 2017, month: Int = 8, day: Int = 8) {
+        
+        let dateComponents = DateComponents(calendar: calendar, timeZone: nil, era: nil, year: year, month: month, day: day)
+        date = calendar.date(from: dateComponents)!
+    }
+}

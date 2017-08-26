5ede8294de5513a4e8ecbac9bb16de0859281c66
Mariano Heredia <mariano@kartjuba.com>
2017-08-26 13:01:02 +1200

    ColorCalendarDayView: Added example view controller.

---
+++ ColorCalendarUITests/ColorCalendarDayViewTests.swift
@@ -0,0 +1,32 @@
+//
+//  ColorCalendarDayViewTests.swift
+//  ColorCalendar
+//
+//  Created by Mariano Heredia on 4/08/17.
+//  Copyright © 2017 Kartjuba. All rights reserved.
+//
+
+import XCTest
+
+class ColorCalendarDayViewTests: XCTestCase {
+    
+    var app: XCUIApplication!
+    
+    override func setUp() {
+        super.setUp()
+        continueAfterFailure = false
+        app = XCUIApplication()
+        app.launch()
+        
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testColorCalendarExists()
+    {
+        app.tables.staticTexts["CalendarDayView"].tap()
+        XCTAssert(app.otherElements["1"].exists)
+    }
+}

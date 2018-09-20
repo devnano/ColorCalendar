17a46397a27830a6bcc77c092c17224bff260215
Mariano Heredia <mariano@kartjuba.com>
2018-09-20 10:39:46 -0300

    Introduced UIColor+Hex.

--- ColorCalendar/R/DefaultColorCalendarPalette.swift
+++ ColorCalendar/R/DefaultColorCalendarPalette.swift
@@ -6,24 +6,6 @@
 //  Copyright © 2018 Kartjuba. All rights reserved.
 //
 
-extension UIColor {
-    convenience init(hex: String, alpha: CGFloat = 1) {
-        assert(hex[hex.startIndex] == "#", "Expected hex string of format #RRGGBB")
-        
-        let scanner = Scanner(string: hex)
-        scanner.scanLocation = 1  // skip #
-        
-        var rgb: UInt32 = 0
-        scanner.scanHexInt32(&rgb)
-        
-        self.init(
-            red:   CGFloat((rgb & 0xFF0000) >> 16)/255.0,
-            green: CGFloat((rgb &   0xFF00) >>  8)/255.0,
-            blue:  CGFloat((rgb &     0xFF)      )/255.0,
-            alpha: alpha)
-    }
-}
-
 // Workaround: r.swift only supports colors from iOS 11 on – Assets Catalog Colors
 struct DefaultColorCalendarPalette {
     static let backgroundColor = UIColor(hex:"#FFFFFF")
---
+++ ColorCalendar/R/UIColor+Hex.swift
@@ -0,0 +1,25 @@
+//
+//  UIColor+Hex.swift
+//  ColorCalendar
+//
+//  Created by Mariano Heredia on 20/09/18.
+//  Copyright © 2018 Kartjuba. All rights reserved.
+//
+
+public extension UIColor {
+    convenience init(hex: String, alpha: CGFloat = 1) {
+        assert(hex[hex.startIndex] == "#", "Expected hex string of format #RRGGBB")
+        
+        let scanner = Scanner(string: hex)
+        scanner.scanLocation = 1  // skip #
+        
+        var rgb: UInt32 = 0
+        scanner.scanHexInt32(&rgb)
+        
+        self.init(
+            red:   CGFloat((rgb & 0xFF0000) >> 16)/255.0,
+            green: CGFloat((rgb &   0xFF00) >>  8)/255.0,
+            blue:  CGFloat((rgb &     0xFF)      )/255.0,
+            alpha: alpha)
+    }
+}
